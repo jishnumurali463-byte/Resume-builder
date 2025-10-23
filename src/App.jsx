a0093b8f691dae9f896c7b54d5f2da83cbcfe252
jishnumurali463-byte <jishnumurali463@gmail.com>
2025-10-23 23:33:32 +0530

    resume builder- commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import { FaHome } from "react-icons/fa";
+
+function App() {
+
+
+  return (
+    <>
+   
+    </>
+  )
+}
+
+export default App
